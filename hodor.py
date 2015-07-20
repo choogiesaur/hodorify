f23f534d28ccb95dc06ae5a1041ce2f6c2d45221
Matt Goldman <robotmlg@gmail.com>
2015-07-20 14:21:48 -0400

    First stab at using syllable counting instead of letter counting

--- hodor.py
+++ hodor.py
@@ -1,5 +1,31 @@
-hodorian_cycle = ['h', 'o', 'd', 'o', 'r']
+from textstat.textstat import textstat
+
+hodorian_syllables = ['Ho', 'dor']
+hodorian_cycle = ['h','o','d','o','r']
 hodorly_decree = ''
+
+#takes in a word, returns the hodorically-correct hodor syllables
+def hodorify_syllables(word):
+
+  new_word = ''
+
+  #count syllables in each word
+  syl_count = int(textstat.syllable_count(word))
+
+  for index in range(0,syl_count):
+
+    hodex = index % 2
+
+    if not word[index].isalpha():
+      new_word += word[index]
+
+    elif word[index].isupper():
+      new_word += hodorian_syllables[hodex]
+
+    else:
+      new_word += hodorian_syllables[hodex].lower()
+
+  return(new_word)
 
 #takes in a word, returns the hodorically-correct hodor
 def hodorify_word(word):
@@ -24,6 +50,6 @@ def hodorify_word(word):
 dictum = str.split(raw_input("\nEnter text to be dictated by his Hodorliness: \n"))
 
 for word in dictum:
-  hodorly_decree += hodorify_word(word) + ' '
+  hodorly_decree += hodorify_syllables(word) + ' '
 
 print('\nHodorly Decree: \n' + str(hodorly_decree))
