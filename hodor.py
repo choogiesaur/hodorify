09dd9cd92dda28a6963d58b96bb4e637d8376781
Firas S <choogiesaur@gmail.com>
2015-07-20 12:49:11 -0400

    refined output
    cleaned up

--- hodor.py
+++ hodor.py
@@ -7,25 +7,21 @@ def hodorify_word(word):
 	for index in range(0,len(word)):
 		
 		hodex = index % 5
-
 		if not word[index].isalpha():
 			new_word += word[index]
+		
 		elif word[index].isupper():
 			new_word += hodorian_cycle[hodex].upper()
+		
 		else:
 			new_word += hodorian_cycle[hodex]
-
-	print(word + ' ----> ' + new_word)
 	return(new_word)
 
-dictum = input("Enter text to be dictated by his Hodorliness: \n")
-
+dictum = input("\nEnter text to be dictated by his Hodorliness: \n")
 words = str.split(dictum)
 
 hodorly_decree = ''
-
-print('Words: \n')
 for word in words:
 	hodorly_decree += hodorify_word(word) + ' '
 
-print('Hodorly Decree: \n' + str(hodorly_decree))
+print('\nHodorly Decree: \n' + str(hodorly_decree))
